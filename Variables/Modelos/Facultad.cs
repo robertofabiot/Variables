269d8fc9aaea8d0510ff43b5e472b9087d18dc07
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-22 07:30:11 -0600

    Add clase facultad

---
+++ Variables/Modelos/Facultad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables.Modelos
+{
+    public class Facultad
+    {
+        public String Nombre;
+        public String Codigo;
+
+        public String MostrarDatos()
+        {
+            return "Facultad " + Nombre + ", Código: " + Codigo;
+    }
+}
